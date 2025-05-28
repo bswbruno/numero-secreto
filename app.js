1ecd5eed8f3d842aa89bde29324d77fc237f6a9d
Bruno Santos <brunusuxx@gmail.com>
2025-05-27 21:02:05 -0300

    mudanças do titulo e gitignore

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
    exibirTextoNaTela("h1", "Jogo do número secreto");
-   exibirTextoNaTela("p", "Escolha um número entre 1 e 30 :");
+   exibirTextoNaTela("p", "Descubra 0 número secreto entre 1 e 30 :");
 }
 
 exibirMensagemInicial();
